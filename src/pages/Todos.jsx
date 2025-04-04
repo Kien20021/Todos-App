30ed82ad06a567fc88e116690ed97493d75dd2fa
Kien20021 <dotrungkien15102k2@gmail.com>
2025-04-04 11:29:00 +0700

    UPdate: delete space

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import logoTodo from "../assets/image/todo-logo.png";
 import clearCompleted from "../assets/image/clear-complete.png";
-
 import ItemTodo from "../components/ItemTodo";
 import Alerts from "../components/alerts/Alerts";
 import ApiServiceTodos from "../services/ApiTodos";
@@ -17,9 +16,7 @@ const Todos = () => {
   });
   const [listTodo, setListTodo] = useState([]);
   const [showAlert, setShowAlert] = useState(false);
-
   const [inforAlerts, setInforAlerts] = useState({});
-
   const handleChangeInputTodo = (e) => {
     const { name, value } = e.target;
     setValInputTodo({
--- src/services/ApiTodos.js
+++ src/services/ApiTodos.js
@@ -1,7 +1,5 @@
 import axios from "axios";
-
 const API_TODOS = import.meta.env.VITE_API_TODOS;
-
 const ApiServiceTodos = {
   apiPostTodo: async (data) => {
     return axios.post(`${API_TODOS}`, data);
@@ -10,5 +8,4 @@ const ApiServiceTodos = {
     return axios.get(API_TODOS);
   },
 };
-
 export default ApiServiceTodos;
